533bc8e4aa75dc341b456d7dc2685c03bdfefd64
Natjama-ma <natjamachanchu@gmail.com>
2024-11-20 05:12:41 +0000

    what

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-print(f"Well, pleased to meet you ,{first_name}{last_name}")
+first_name = input("Hey, what's your first name? : ")
+last_name = input("what's your last name? : ")
+print(f"Well, pleased to meet you, {first_name} {last_name}") 
