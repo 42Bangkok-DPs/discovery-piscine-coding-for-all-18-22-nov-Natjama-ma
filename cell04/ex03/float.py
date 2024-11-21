31de0ebea49d0524dd48c4748600171fbe1069b2
Natjama-ma <natjamachanchu@gmail.com>
2024-11-21 06:22:26 +0000

    fl

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -7,6 +7,6 @@ try:
     if num.is_integer():
         print("This number is an integer.")
     else:
-        print("This number is a float.")
+        print("This number is a decimal.")
 except ValueError:
     print("This is not a valid number.")
